1a94cf655a9a921b82b212b2ff65b0211fade56a
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2024-05-01 23:49:00 +0100

    Add files via upload

---
+++ dataStructures/spreadAndRest.js
@@ -0,0 +1,38 @@
+// Join arrays, objects using the rest operator
+const fruits = ['apple', 'pear', 'plum']
+const berries = ['blueberry', 'strawberry']
+const fruitsAndBerries = [...fruits, ...berries] // concatenate
+console.log(fruitsAndBerries); // outputs a single array
+
+const flying = { wings: 2 }
+const car = { wheels: 4 }
+const flyingCar = {...flying, ...car}
+console.log(flyingCar) // {wings: 2, wheels: 4}
+
+
+// Add new members to arrays without using the push() method
+let veggies = ['onion', 'parsley'];
+veggies = [...veggies, 'carrot', 'beetroot'];
+console.log(veggies);
+
+// Convert a string to an array using the spread operator
+const greeting = "Hello";
+const arrayOfChars = [...greeting];
+console.log(arrayOfChars); //  ['H', 'e', 'l', 'l', 'o']
+
+
+//Copy either an object or an array into a separate one
+const car1 = {
+    speed: 200,
+    color: 'yellow'
+}
+const car2 = {...car1}
+
+car1.speed = 201
+
+console.log(car1.speed, car2.speed)
+
+const fruits1 = ['apples', 'pears']
+const fruits2 = [...fruits1]
+fruits1.pop()
+console.log(fruits1, "not", fruits2)
