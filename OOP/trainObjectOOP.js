c6c2dd101d4abf106027253466785a3cc53632e5
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2024-05-01 23:48:41 +0100

    Updated content

--- OOP/trainObjectOOP.js
+++ OOP/trainObjectOOP.js
@@ -1,3 +1,5 @@
+// Class, inheritance, constructor and methods
+
 class Train {
   constructor(color, lightsOn) {
     this.color = color;
@@ -79,3 +81,23 @@ var boxingGym = new Gym("7-22", "right corner", "left corner");
 console.log(boxingGym.openHrs);
 console.log(boxingGym.stationaryBike);
 console.log(boxingGym.treadmill);
+
+// Default Parameters
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6
